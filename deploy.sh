fc844caa92e847c7bc1fdbedf82ea0afc8aa55f9
wjisi <7042783@qq.com>
2025-10-27 15:26:43 +0800

    add updates command

--- deploy.sh
+++ deploy.sh
@@ -10,4 +10,7 @@ npm run build
 # 拷贝到服务器
 rsync -avzr -e 'ssh -p 6422' dist/ front@58.243.201.56:~/frontcode/jdid_cn/dist
 
+# 同步
+#param='web=jdid_cn';job="front-code-sync";token="115574c30a3084ab6aac18e1b005842a68";curl -X POST "https://whjenkins.jccdex.cn:8443/job/${job}/buildWithParameters?${param}" --user "afd:${token}"
+
 #正式环境 https://jdid.cn
